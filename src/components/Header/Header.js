4e120e2ce4e1c40c9176d26098ec8153cc650c49
bchang-93 <bdc11122@gmail.com>
2018-11-29 23:51:12 -0800

    design for logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Header(props) {
   return (
     <div className="Header">
       <a className="jumbotron_title" href="/">{props.title}</a>
+        <button className="sign_out">
+          <span className="sign_out_icon"><FontAwesomeIcon icon="sign-out-alt" /></span>
+        </button>
     </div>
   );
 };
