214de8b13566fb5b86dc21088902645b3c7e68d6
bchang-93 <bdc11122@gmail.com>
2018-11-30 14:02:54 -0800

    add avatar prop

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,21 +3,25 @@ import './Post.css'
 import PostForm from '../PostForm/PostForm'
 
 export default function Post(props) {
-  console.log(props)
+  console.log(props.creator.avatar)
   return(
     <div className="post_panel">
       <div className="post_info">
         <div className="post_heading">
         <div className="create-by">
+          <img
+          className="avatar"
+          src={props.creator.avatar}
+          alt="user-avatar"/>
           <span className="creator">
-            by {props.creator.firstName} {props.creator.lastName}
+            {props.creator.firstName} {props.creator.lastName}
           </span>
         </div>
           <h2 className="dream_title">{props.title}</h2>
         </div>
         <p className="story">{props.content}</p>
       </div>
-      <PostForm />
+      <PostForm comments={props.comments}/>
     </div>
   );
 };
