d2a7559d3b953686cdbe9137ecb5347938c30e3f
bchang-93 <bdc11122@gmail.com>
2018-11-29 23:52:57 -0800

    display creator in dream post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,10 +3,16 @@ import './Post.css'
 import PostForm from '../PostForm/PostForm'
 
 export default function Post(props) {
+  console.log(props)
   return(
     <div className="post_panel">
       <div className="post_info">
         <div className="post_heading">
+        <div className="create-by">
+          <span className="creator">
+            by {props.creator.firstName} {props.creator.lastName}
+          </span>
+        </div>
           <h2 className="dream_title">{props.title}</h2>
         </div>
         <p className="story">{props.content}</p>
