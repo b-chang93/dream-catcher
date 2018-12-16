d2e4d8bf88a66a3f2ec6d70b5200cece7fe7564b
bchang-93 <bdc11122@gmail.com>
2018-12-15 22:03:53 -0800

    html css extends full page length and update component to render header

--- src/components/AppForm/AppForm.js
+++ src/components/AppForm/AppForm.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './AppForm.css'
 import HomePage from '../HomePage/HomePage'
+import Header from '../Header/Header.js'
 
 export default class AppForm extends React.Component {
   render() {
     return (
       <div className="AppForm">
+        <Header title="Dream Catcher"/>
         <HomePage {...this.props} />
       </div>
     );
